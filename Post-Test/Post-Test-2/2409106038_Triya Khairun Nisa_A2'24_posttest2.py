c2142a670452f6734bb3e9ed37d02f831fa802b2
Triya Khairun Nisa <130401355+3yakhansa@users.noreply.github.com>
2024-09-16 12:49:35 +0800

    Add files via upload

---
+++ Post-Test/Post-Test-2/2409106038_Triya Khairun Nisa_A2'24_posttest2.py
@@ -0,0 +1,17 @@
+#Nama dan NIM
+nama = "Triya Khairun Nisa"
+nim = 2409106038
+harga = 400000
+print(nama + " " + "dengan NIM" + " " + str(nim) + " " + "ingin membeli beras dengan harga" + " " + "Rp." + str(harga))
+
+#Harga Beras Mawar
+berasMawar = (harga) - (harga)*(11/100)
+print("Jika ia membeli beras Mawar ia akan membayar" + " " + "Rp." + str(berasMawar) + " " + "setelah mendapat diskon 11%")
+
+#Harga Beras Sania
+berasSania = (harga) - (harga)*(14/100)
+print("Jika ia membeli beras Sania ia akan membayar" + " " + "Rp." + str(berasSania) + " " + "setelah mendapat diskon 14%")
+
+#Harga Beras Maknyus
+berasMaknyus = (harga) - (harga)*(17/100)
+print("Jika ia membeli beras Maknyus ia akan membayar" + " " + "Rp." + str(berasMaknyus) + " " + "setelah mendapat diskon 17%")
